b8af3d578936bdc458a78ad20fc0cce684ec0e62
Vladimir <vlad1im1r0195@gmail.com>
2020-01-21 01:21:25 +0300

    добавлена рекурсия

--- vanilla/index3.js
+++ vanilla/index3.js
@@ -3,24 +3,32 @@ const longestPalindrome = (string) => {
 
   let strArray = string.split('')
   let palindromes = []
-
-
+  let longestPalindrome = ''
 
   const isPalindrome = (el, nexel) => {
     let elem = el + nexel
-    if (elem === elem.split('').reverse().join('')) {
+    if (elem === elem.split('').reverse().join('') && elem.length > 2) {
       palindromes.push(elem)
     }
-
     return elem
+  }
 
+  const getPalindrome = (array, n) => {
+    if (n <= 2) {
+      return
+    } else {
+      array.reduce((el, nexel) => isPalindrome(el, nexel))
+      console.log(array)
+      array.shift()
+      return getPalindrome(array, n - 1)
+    }
 
   }
 
-  strArray.reduce((el, nexel) => isPalindrome(el, nexel))
 
-  return palindromes.join('')
-  // isPalindrome(el + nexel)
+  getPalindrome(strArray, strArray.length)
+  return longestPalindrome = palindromes.filter(el => el.length === Math.max(...palindromes.map(str => str.length))).join('')
+
 
 
 }
